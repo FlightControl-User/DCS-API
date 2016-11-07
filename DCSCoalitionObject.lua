599c1e273f8c39c345b2063cd2656eb3c60a6376
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-11-07 09:45:14 +0100

    Moved coalition namespace to separate script, added 'getCountryCoalition'

--- DCSCoalitionObject.lua
+++ DCSCoalitionObject.lua
@@ -4,16 +4,6 @@
 --- @type CoalitionObject
 -- @extends DCSObject#Object
 
---- @type coalition
--- @field #coalition.side side
-
---- @type coalition.side
--- @field NEUTRAL
--- @field RED
--- @field BLUE
-
-coalition = {} --#coalition
-
 --- Returns coalition of the object.
 -- @function [parent=#CoalitionObject] getCoalition
 -- @param #CoalitionObject self
