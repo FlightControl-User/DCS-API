a3262d7319f0c5ee8dfa46cd1160a26251efb9b9
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-12-22 15:31:14 +0100

    Started #2: added getUserFlag, setUserFlag, getZone

--- DCStrigger.lua
+++ DCStrigger.lua
@@ -2,7 +2,30 @@
 -- @module DCStrigger
 
 
-trigger = {} --#timer
+
+---
+-- @type trigger
+trigger = {}
 
 
-  
+---
+-- @type trigger.misc
+trigger.misc = {}
+
+
+--- Returns the value of a user flag.
+-- @function trigger.misc.getUserFlag 
+-- @param #string flagName
+-- @return #number
+ 
+
+--- Sets a user flag to the specified value. 
+-- @function [parent=#trigger.misc] setUserFlag
+-- @param #string flagName
+-- @param flagValue New value for flag (boolean or number)
+
+
+--- Returns a trigger zone table of a given name.
+-- @function [parent=#trigger.misc] getZone
+-- @param #string zoneName
+-- @return DCSZone#Zone
