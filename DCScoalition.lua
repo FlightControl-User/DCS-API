2e6c009b38507fd1033ec55883d38081b93c0c2c
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-07 17:24:03 +0100

    (#6) Added coalition.getGroups

--- DCScoalition.lua
+++ DCScoalition.lua
@@ -13,4 +13,12 @@
 -- @param #number countryId
 -- @return #number coalitionId
 
+---
+-- Returns a table of group objects belonging to the specified coalition.
+-- If the groupCategory enumerator is provided the table will only contain groups that belong to the specified category.
+-- If this optional variable is not provided, all group types will be returned.
+-- @function [parent=#coalition] getGroups
+-- @param #number coalitionId, #number groupCategory
+-- @return #list<DCSGroup#Group>
+
 coalition = {} --#coalition
